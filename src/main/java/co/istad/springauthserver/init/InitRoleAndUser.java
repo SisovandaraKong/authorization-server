e4d87814253978b77fe6785026284d2f0c5af40b
SisovandaraKong <sisovandara@gmail.com>
2025-12-02 21:15:17 +0700

    auth server all in

---
+++ src/main/java/co/istad/springauthserver/init/InitRoleAndUser.java
@@ -0,0 +1,44 @@
+package co.istad.springauthserver.init;
+
+import co.istad.springauthserver.domain.Role;
+import co.istad.springauthserver.domain.User;
+import co.istad.springauthserver.feature.role.RoleRepository;
+import co.istad.springauthserver.feature.user.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Component
+@RequiredArgsConstructor
+public class InitRoleAndUser {
+
+    private final RoleRepository repo;
+    private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        Role role = Role.builder()
+                .uuid(UUID.randomUUID().toString())
+                .role("USER")
+                .build();
+
+        User user = User.builder()
+                .uuid(UUID.randomUUID().toString())
+                .roles(List.of(role))
+                .email("sisovandara@gmail.com")
+                .password(passwordEncoder.encode("dara1234"))
+                .username("kongsisovandara")
+                .familyName("Kong")
+                .givenName("Sisovandara")
+                .isEnabled(true)
+                .build();
+        repo.save(role);
+        userRepo.save(user);
+    }
+}
